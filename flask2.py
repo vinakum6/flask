b539fefd296d237c20ad40bd42149ffb0d2c6d85
vinakum6 <53523817+vinakum6@users.noreply.github.com>
2020-05-05 18:50:18 +0100

    Mofified the code to fix the bugs

--- flask2.py
+++ flask2.py
@@ -1,8 +1,11 @@
-from flask import Flask,add_url_rule
+from flask import Flask
 
 app=Flask(__name__)
 
 def index():
     return 'Welcome to my second program with different decorator approach'
 
-@app.add_url_rule('/','index'index)
+app.add_url_rule('/','index',index)
+
+if __name__=='__main__':
+    app.run(debug=True)
